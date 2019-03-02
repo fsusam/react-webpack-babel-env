4ab95bdf152ad7d1a040d694cd26468b3424fa95
fsusam <fsusam@gmail.com>
2019-03-02 18:39:54 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/App';
+
+import 'bootstrap';
+import "index.scss";
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById("root")
+);
